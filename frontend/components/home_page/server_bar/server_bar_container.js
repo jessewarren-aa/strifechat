1420f7ef255ebda3eef1246e34363f7e821f1b6b
Jesse <jesse.mwarren@yahoo.com>
2019-12-31 11:26:15 -0800

    Filter servers by user presence

--- frontend/components/home_page/server_bar/server_bar_container.js
+++ frontend/components/home_page/server_bar/server_bar_container.js
@@ -4,9 +4,39 @@ import ServerBar from './server_bar';
 import { sendGetServers } from '../../../actions/servers_actions'
 
 
+const userInServer = (userId, serverId, serverUsers) => {
+  const serverUserObjects = Object.values(serverUsers).filter(serverUser => {
+    if (serverUser.user_id === userId) {
+      if (serverUser.server_id === serverId) {
+        return true
+      } else {
+        return false
+      }
+    } else {
+      return false
+    }
+  })
+
+  if (serverUserObjects.length > 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
 const mapStateToProps = (state, ownProps) => {
+  const currentUserId = state.session.currentUser
+  const filteredServers = {}
+  Object.values(state.entities.servers).forEach(server => {
+    if (userInServer(currentUserId, server.id, state.entities.serverUsers)) {
+      filteredServers[server.id] = server
+    }
+  })
+
   return {
-    servers: state.entities.servers
+    servers: filteredServers,
+    users: state.entities.users,
+    serverUsers: state.entities.serverUsers
   }
 };
 
