9862092dd4de99023f59f9556d6aade148b836a3
Jesse <jesse.mwarren@yahoo.com>
2020-01-03 11:29:19 -0800

    Heroku precompile

--- frontend/components/home_page/server_channels/server_footer/server_footer_container.js
+++ frontend/components/home_page/server_channels/server_footer/server_footer_container.js
@@ -3,6 +3,7 @@ import Serverfooter from './server_footer';
 import { logout } from '../../../../actions/session_actions';
 
 import { get_dm_users} from '../../../../actions/message_actions'
+import { sendUpdateUser } from '../../../../actions/user_actions'
 
 
 const mapStateToProps = (state) => {
@@ -10,13 +11,16 @@ const mapStateToProps = (state) => {
 
   return {
     friendCode: user ? user.friend_code : null,
-    currentStatus: user ? user.current_status : "OFFLINE"
+    currentStatus: user ? user.current_status : "OFFLINE",
+    users: state.entities.users,
+    currentUser: state.session.currentUser
   }
 };
 
 const mapDispatchToProps = (dispatch) => ({
   logout: () => dispatch(logout()),
   getDirectMessageUsers: () => dispatch(get_dm_users()),
+  sendUpdateUser: (user) => dispatch(sendUpdateUser(user))
 });
 
 export default connect(
