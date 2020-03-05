3921245c265b8ea2da196ef07e5a0a2da94492c7
Jesse <jesse.mwarren@yahoo.com>
2020-03-05 12:03:58 -0800

    Add ActionCable notification

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -18,30 +18,25 @@ class App extends React.Component {
   }
 
   render () {
-    return <div>
-      <Switch>
-        <Route 
-          exact path="/" 
-          component={ SplashPageContainer } />
-        <AuthRoute 
-          exact path="/login" 
-          component={ LoginFormContainer } />
-        <AuthRoute 
-          exact path="/signup" 
-          component={ SignupFormContainer } />
-        <ProtectedRoute 
-          path="/channels" 
-          component={ FourOhFourWrapperContainer } />
-        <Route
-          exact path="/terms"
-          component={TermsContainer} />
-        <Route
-          exact path="/privacy"
-          component={PrivacyContainer} />
-        <Route path="*" 
-          component={FourOhFourContainer} />
-      </Switch>
-    </div>
+    return (
+      <div>
+        <div id="known-issues" className="text-center">
+          <h4>Developer note: Live chat is not currently active. ActionCable will be coming soon!</h4>
+        </div>
+        <Switch>
+          <Route exact path="/" component={SplashPageContainer} />
+          <AuthRoute exact path="/login" component={LoginFormContainer} />
+          <AuthRoute exact path="/signup" component={SignupFormContainer} />
+          <ProtectedRoute
+            path="/channels"
+            component={FourOhFourWrapperContainer}
+          />
+          <Route exact path="/terms" component={TermsContainer} />
+          <Route exact path="/privacy" component={PrivacyContainer} />
+          <Route path="*" component={FourOhFourContainer} />
+        </Switch>
+      </div>
+    );
   }
 }
 
